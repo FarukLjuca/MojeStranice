56634f23b3a1583eb1cedd75399951a82fd1c8bb
Faruk Ljuca <fljuca1@etf.unsa.ba>
2015-05-25 12:57:19 +0200

    lalala

--- Spirale/milenijumsoft.sql
+++ Spirale/milenijumsoft.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: May 23, 2015 at 06:02 PM
+-- Generation Time: May 25, 2015 at 12:37 PM
 -- Server version: 5.6.17
 -- PHP Version: 5.5.12
 
@@ -33,16 +33,19 @@ CREATE TABLE IF NOT EXISTS `novosti` (
   `detaljnijiTekst` text COLLATE utf8_slovenian_ci,
   `autor` varchar(45) COLLATE utf8_slovenian_ci NOT NULL,
   `datumObjave` timestamp NOT NULL,
+  `slika` text COLLATE utf8_slovenian_ci,
   PRIMARY KEY (`idNovosti`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_slovenian_ci AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_slovenian_ci AUTO_INCREMENT=7 ;
 
 --
 -- Dumping data for table `novosti`
 --
 
-INSERT INTO `novosti` (`idNovosti`, `naslov`, `tekst`, `detaljnijiTekst`, `autor`, `datumObjave`) VALUES
-(1, 'Takmičenje iz matematike', 'Takmičenje iz matematike će se održati u petak', 'neki detaljniji tekst', 'Ljuca Faruk', '2015-05-21 20:06:18'),
-(2, 'Takmičenje iz programiranja', 'Ovaj je bez detaljnije', NULL, 'Ljuca Faruk', '2015-05-21 20:06:54');
+INSERT INTO `novosti` (`idNovosti`, `naslov`, `tekst`, `detaljnijiTekst`, `autor`, `datumObjave`, `slika`) VALUES
+(3, 'Pluralsight je najbolji', 'Pluralsight, stranica na kojoj se može naći najbolji trening za buduće programemre je ubjedljivo najvolja...', 'Pluralsight, stranica na kojoj se može naći najbolji trening za buduće programemre je ubjedljivo najvolja. Duge dobra stranica je Microsoft Virtual Academy, međutim na ovoj stanici nema treninga za tehnologije koje su konkurentne Microsoftu (Android, Java...)', 'Ljuca Faruk', '2015-05-25 10:21:35', 'http://golfstinks.com/blog/wp-content/uploads/2013/02/white-tail-deer.jpg'),
+(4, 'Pluralsight je najbolji', 'Pluralsight, stranica na kojoj se može naći najbolji trening za buduće programemre je ubjedljivo najvolja...', 'Pluralsight, stranica na kojoj se može naći najbolji trening za buduće programemre je ubjedljivo najvolja. Duge dobra stranica je Microsoft Virtual Academy, međutim na ovoj stanici nema treninga za tehnologije koje su konkurentne Microsoftu (Android, Java...)', 'Ljuca Faruk', '2015-05-25 10:22:11', NULL),
+(5, 'Pluralsight je najbolji', 'Pluralsight, stranica na kojoj se može naći najbolji trening za buduće programemre je ubjedljivo najvolja...', NULL, 'Ljuca Faruk', '2015-05-25 10:22:41', 'http://golfstinks.com/blog/wp-content/uploads/2013/02/white-tail-deer.jpg'),
+(6, 'Pluralsight je najbolji', 'Pluralsight, stranica na kojoj se može naći najbolji trening za buduće programemre je ubjedljivo najvolja...', NULL, 'Ljuca Faruk', '2015-05-25 10:23:09', NULL);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
